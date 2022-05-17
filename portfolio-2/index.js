1ba589a28b390113a0c9fccb2b37f2c967908636
ozgeAtik <ozgeatik4@gmail.com>
2022-05-17 19:28:41 +0300

    portfolio

---
+++ portfolio-2/index.js
@@ -0,0 +1,27 @@
+projects = [
+  {
+    title: "Quizzical",
+    image: "./quizzical.png",
+    link: "https://react-quizzicall.netlify.app",
+  },
+  {
+    title: "Color Picker",
+    image: "./color-picker.png",
+    link: "https://color-picker5.netlify.app",
+  },
+  {
+    title: "To-Do App with React",
+    image: "./todo-app.png",
+    link: "https://simple-todo-with-react.netlify.app",
+  },
+];
+
+projects.map((p) => {
+  document.getElementById("projects-content").innerHTML += `
+    <div class="single-project">
+        <h3>${p.title}</h3>
+        <img src=${p.image} />
+        <a href=${p.link} target=_blank>Visit...</a>
+    </div>
+    `;
+});
